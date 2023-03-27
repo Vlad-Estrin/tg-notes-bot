a75da01784f54299147745174ac3a7e95823e17d
Vlad Estrin <estrynvladislav@gmail.com>
2023-03-27 16:09:42 +0200

    fixed service

--- src/main/java/com/gmail/estrynvladislav/tgnotesbot/service/BotService.java
+++ src/main/java/com/gmail/estrynvladislav/tgnotesbot/service/BotService.java
@@ -4,7 +4,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 public interface BotService {
     SendMessage getStartMsg(final long chatId);
-
     SendMessage getStopMsg(final long chatId);
     SendMessage getDefaultMessage(final long chatId);
 }
